a066a39874af3f3f64b1fb0b3fcef249d6cc3baf
Mitchell Jaqueline <mitchell@su-c3.msk.21-school.ru>
2022-08-25 14:50:38 +0300

    ,

--- shared.c
+++ shared.c
@@ -1,18 +1,59 @@
 #include "database.h"
 
-
-tb_entity *select(FILE *db, int id) {
-
+void select(select_q *query) {
+    printf("[DEBUG] select\n");
+    switch (query->table_id) {
+        case MODULES_TB_ID: read_from_database_modules(query->columns);
+            break;
+        case LEVELS_TB_ID: read_from_database_levels(query->columns);
+            break;
+        case STATUSSES_TB_ID: read_from_database_statusses(query->columns);
+            break;
+        default:
+            break;
+    }
 }
 
-int delete(FILE *db, int id) {
-
-}
-
-int insert(FILE *db, tb_entity *entity) {
-
-}
-
-int update(FILE *db, int id, tb_entity *entity) {
+void delete(delete_q *query) {
+    printf("[DEBUG] delete\n");
+    switch(query->table.id) {
+        case MODULES_TB_ID: delete_from_database_modules(query->);
+            break;
+        case LEVELS_TB_ID: delete_from_database_levels(query->);
+            break;
+        case STATUSSES_TB_ID: delete_from_database_statusses(query->);
+            break;
+        default:
+            break; 
+    }
     
+}
+
+void insert(insert_q *query) {
+    printf("[DEBUG] insert\n");
+    switch (query->table.id) {
+        case MODULES_TB_ID: insert_from_database_modules(query->);
+            break;
+        case LEVELS_TB_ID: insert_from_database_levels(query->);
+            break;
+        case STATUSSES_TB_ID: insert_from_database_statusses(query->);
+            break;
+        default: 
+            break;
+    }
+}
+
+int update(update_q *query) {
+    printf("[DEBUG] update\n");
+    switch (query->table.id) {
+        case MODULES_TB_ID: update_from_database_modules(query->);
+            break;
+        case LEVELS_TB_ID: update_from_database_levels(query->);;
+            break;
+        case STATUSSES_TB_ID:update_from_database_statusses(query->);;
+            break;
+        default:
+            break; 
+    }
+
 }
